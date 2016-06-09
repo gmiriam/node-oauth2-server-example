5fc5626d915e4d32a3c3d5e58ec6f5a5beed1055
gmiriam <gmiriam87@gmail.com>
2016-06-09 20:22:34 +0100

    Añade soporte cors y personaliza puerto

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    oauthserver = require('oauth2-server');
+    oauthserver = require('oauth2-server'),
+    cors = require('cors');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.oauth = oauthserver({
     model: require('./model.js'),
@@ -22,4 +24,4 @@ app.get('/', app.oauth.authorise(), function (req, res) {
 
 app.use(app.oauth.errorHandler());
 
-app.listen(3000);
+app.listen(3010);
